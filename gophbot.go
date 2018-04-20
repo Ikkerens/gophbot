d9ffffe4dd46154f242c0bc7e2e84746305ac85f
Rens Rikkerink <github@ikkerens.com>
2018-04-20 22:55:10 +0200

    Lint/build fixes

--- gophbot.go
+++ gophbot.go
@@ -6,6 +6,7 @@ import (
 	"syscall"
 )
 
+// Start is the main entry point for the bot, outside of the main package to allow external packages to call back
 func Start() {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
