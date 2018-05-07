0826f572d83df26cf573349835bda449809a1bf1
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2018-05-07 16:50:42 +0200

    Updated bot status updater

--- status.go
+++ status.go
@@ -1,7 +1,55 @@
 package gophbot
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/hako/durafmt"
+)
+
+var startup = time.Now()
+var statusBuilders = []func() string{
+	getGuildCount,
+	getMemberCount,
+	timeSinceStart,
+}
+
 func statusLoop() {
-	for _, discord := range sessions {
-		discord.UpdateStatus(0, "with Development")
+	next := 0
+	for {
+		status := statusBuilders[next]()
+		next = (next + 1) % len(statusBuilders)
+
+		for _, discord := range sessions {
+			discord.UpdateStatus(0, status)
+		}
+		time.Sleep(30 * time.Second)
 	}
+}
+
+func getGuildCount() string {
+	guilds := 0
+	for _, discord := range sessions {
+		guilds += len(discord.State.Guilds)
+	}
+
+	return fmt.Sprintf("in %d servers", guilds)
+}
+
+func getMemberCount() string {
+	members := 0
+	for _, discord := range sessions {
+		discord.State.Lock()
+		for _, guild := range discord.State.Guilds {
+
+			members += guild.MemberCount
+		}
+		discord.State.Unlock()
+	}
+
+	return fmt.Sprintf("with %d users", members)
+}
+
+func timeSinceStart() string {
+	return "for " + durafmt.ParseShort(time.Since(startup)).String()
 }
